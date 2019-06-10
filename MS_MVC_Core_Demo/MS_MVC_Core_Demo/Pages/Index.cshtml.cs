1d46fa53c0666cd0da50882ee9cb4afeda9e96bf
uberknot <tfredrickson@yahoo.com>
2019-06-10 10:07:19 -0400

    Initial check in

---
+++ MS_MVC_Core_Demo/MS_MVC_Core_Demo/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MS_MVC_Core_Demo.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
